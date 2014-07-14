acd7b8a463beb71e04db13775fc0fd3623881aa8
Dannes Wessels <dannes@exist-db.org>
2014-07-14 22:10:53 +0200

    Added report for store

--- java/src/org/exist/mongodb/xquery/gridfs/Store.java
+++ java/src/org/exist/mongodb/xquery/gridfs/Store.java
@@ -22,8 +22,18 @@ package org.exist.mongodb.xquery.gridfs;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSFile;
 import com.mongodb.gridfs.GridFSInputFile;
+import java.util.List;
+import javax.jms.JMSException;
+import javax.naming.Context;
+import javax.xml.datatype.Duration;
+import org.apache.commons.lang3.StringUtils;
 import org.exist.dom.QName;
+import org.exist.memtree.DocumentImpl;
+import org.exist.memtree.MemTreeBuilder;
+import org.exist.memtree.NodeImpl;
+import org.exist.messaging.shared.Report;
 import org.exist.mongodb.shared.Constants;
 import org.exist.mongodb.shared.MongodbClientStore;
 import org.exist.mongodb.xquery.GridfsModule;
@@ -32,6 +42,7 @@ import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.DateTimeValue;
 import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.FunctionReturnSequenceType;
 import org.exist.xquery.value.Sequence;
@@ -41,16 +52,12 @@ import org.exist.xquery.value.Type;
 
 /**
  * Implementation gridfs:connect() functions
- * 
+ *
  * @author Dannes Wessels
  */
-
 public class Store extends BasicFunction {
-    
-    
-    
-    public final static FunctionSignature signatures[] = {
 
+    public final static FunctionSignature signatures[] = {
         new FunctionSignature(
         new QName("store", GridfsModule.NAMESPACE_URI, GridfsModule.PREFIX),
         "Store document into Gridfs",
@@ -61,11 +68,9 @@ public class Store extends BasicFunction {
             new FunctionParameterSequenceType("filename", Type.STRING, Cardinality.ONE, "Name of document"),
             new FunctionParameterSequenceType("contentType", Type.STRING, Cardinality.ONE, "COntent type"),
             new FunctionParameterSequenceType("content", Type.STRING, Cardinality.ONE, "Content TODO")
-            },
-        new FunctionReturnSequenceType(Type.STRING, Cardinality.ONE, "an ID")
-        ),
-        
-    };
+        },
+        new FunctionReturnSequenceType(Type.ELEMENT, Cardinality.ONE, "an ID")
+        ),};
 
     public Store(XQueryContext context, FunctionSignature signature) {
         super(context, signature);
@@ -84,7 +89,6 @@ public class Store extends BasicFunction {
         }
 
         // Get parameters
-        // TODO handle (), etc
         String id = args[0].itemAt(0).getStringValue();
         String dbname = args[1].itemAt(0).getStringValue();
         String collection = args[2].itemAt(0).getStringValue();
@@ -92,22 +96,77 @@ public class Store extends BasicFunction {
         String contentType = args[4].itemAt(0).getStringValue();
         String content = args[5].itemAt(0).getStringValue();
 
-        // Get
+        // Get appropriate Mongodb client
         MongoClient client = MongodbClientStore.getInstance().get(id);
+
+        // Get database
         DB db = client.getDB(dbname);
 
+        // Creates a GridFS instance for the specified bucket
         GridFS gfs = new GridFS(db, collection);
 
+        // Create file entry
         GridFSInputFile gfsFile = gfs.createFile(content.getBytes());
 
+        // Set meta data
         gfsFile.setFilename(documentName);
         gfsFile.setContentType(contentType);
+
+        // Save and persist
         gfsFile.save();
 
+        System.out.println(gfsFile.toString());
+
         // Report identifier
-        return new StringValue(gfsFile.getId().toString());
+        return getReport(gfsFile);
 
+    }
 
+    NodeImpl getReport(GridFSFile gfsFile) throws XPathException {
 
-    }    
+        MemTreeBuilder builder = new MemTreeBuilder();
+        builder.startDocument();
+
+        // start root element
+        int nodeNr = builder.startElement("", "GridFSFile", "GridFSFile", null);
+
+        // Some identities
+        add(builder, "id", "" + gfsFile.getId());
+        add(builder, "filename", gfsFile.getFilename());
+        
+        List<String> aliases = gfsFile.getAliases();
+        if (aliases != null) {
+            for (String alias : gfsFile.getAliases()) {
+                add(builder, "alias", alias);
+            }
+        }
+
+        // mimetype
+        add(builder, "contentType",gfsFile.getContentType());
+        
+        // sizes
+        add(builder, "length", "" + gfsFile.getLength());
+        add(builder, "chunkSize", "" + gfsFile.getChunkSize());
+        add(builder, "numberOfChunks", "" + gfsFile.numChunks());
+        
+        // more meta data
+        add(builder, "uploadDate", "" + (new DateTimeValue(gfsFile.getUploadDate()).getStringValue()));
+        add(builder, "md5", gfsFile.getMD5());
+        
+        // finish root element
+        builder.endElement();
+
+        // return result
+        return ((DocumentImpl) builder.getDocument()).getNode(nodeNr);
+    }
+
+    private void add(MemTreeBuilder builder, String elementName, String value) {
+
+        if (StringUtils.isNotBlank(value) && StringUtils.isNotBlank(elementName)) {
+            builder.startElement("", elementName, elementName, null);
+            builder.characters(value);
+            builder.endElement();
+        }
+
+    }
 }
