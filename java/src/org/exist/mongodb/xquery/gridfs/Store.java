e1a9e7f24f3a03a69ea8bd3354ceee6860a43ab3
Dannes Wessels <dannes@exist-db.org>
2014-07-15 22:27:03 +0200

    WiP: can store arbitrary documents, not efficient though

--- java/src/org/exist/mongodb/xquery/gridfs/Store.java
+++ java/src/org/exist/mongodb/xquery/gridfs/Store.java
@@ -28,6 +28,7 @@ import java.util.List;
 import javax.jms.JMSException;
 import javax.naming.Context;
 import javax.xml.datatype.Duration;
+import javax.xml.transform.stream.StreamSource;
 import org.apache.commons.lang3.StringUtils;
 import org.exist.dom.QName;
 import org.exist.memtree.DocumentImpl;
@@ -37,14 +38,18 @@ import org.exist.messaging.shared.Report;
 import org.exist.mongodb.shared.Constants;
 import org.exist.mongodb.shared.MongodbClientStore;
 import org.exist.mongodb.xquery.GridfsModule;
+import org.exist.util.MimeTable;
+import org.exist.util.MimeType;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
+import org.exist.xquery.functions.validation.Shared;
 import org.exist.xquery.value.DateTimeValue;
 import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.StringValue;
@@ -66,8 +71,8 @@ public class Store extends BasicFunction {
             new FunctionParameterSequenceType("database", Type.STRING, Cardinality.ONE, "database"),
             new FunctionParameterSequenceType("collection", Type.STRING, Cardinality.ONE, "Collection"),
             new FunctionParameterSequenceType("filename", Type.STRING, Cardinality.ONE, "Name of document"),
-            new FunctionParameterSequenceType("contentType", Type.STRING, Cardinality.ONE, "COntent type"),
-            new FunctionParameterSequenceType("content", Type.STRING, Cardinality.ONE, "Content TODO")
+            new FunctionParameterSequenceType("contentType", Type.STRING, Cardinality.ONE, "Document Content type, use () for mime-type based on file extension"),
+            new FunctionParameterSequenceType("content", Type.ITEM, Cardinality.ONE, "Document content as node() or  base64-binary")
         },
         new FunctionReturnSequenceType(Type.ELEMENT, Cardinality.ONE, "an ID")
         ),};
@@ -88,37 +93,44 @@ public class Store extends BasicFunction {
             throw ex;
         }
 
-        // Get parameters
-        String id = args[0].itemAt(0).getStringValue();
-        String dbname = args[1].itemAt(0).getStringValue();
-        String collection = args[2].itemAt(0).getStringValue();
-        String documentName = args[3].itemAt(0).getStringValue();
-        String contentType = args[4].itemAt(0).getStringValue();
-        String content = args[5].itemAt(0).getStringValue();
+        try {
 
-        // Get appropriate Mongodb client
-        MongoClient client = MongodbClientStore.getInstance().get(id);
+            // Get parameters
+            String id = args[0].itemAt(0).getStringValue();
+            String dbname = args[1].itemAt(0).getStringValue();
+            String bucket = args[2].itemAt(0).getStringValue();
+            String documentName = args[3].itemAt(0).getStringValue();
+            String contentType = "aaa"; //args[4].itemAt(0).getStringValue();
 
-        // Get database
-        DB db = client.getDB(dbname);
+            // content: File object, doc() element, base64...
+            Item content = args[5].itemAt(0);
 
-        // Creates a GridFS instance for the specified bucket
-        GridFS gfs = new GridFS(db, collection);
+            StreamSource ss = Shared.getStreamSource(content, context);
 
-        // Create file entry
-        GridFSInputFile gfsFile = gfs.createFile(content.getBytes());
+            // Get appropriate Mongodb client
+            MongoClient client = MongodbClientStore.getInstance().get(id);
 
-        // Set meta data
-        gfsFile.setFilename(documentName);
-        gfsFile.setContentType(contentType);
+            // Get database
+            DB db = client.getDB(dbname);
 
-        // Save and persist
-        gfsFile.save();
+            // Creates a GridFS instance for the specified bucket
+            GridFS gfs = new GridFS(db, bucket);
 
-        System.out.println(gfsFile.toString());
+            // Create file entry
+            GridFSInputFile gfsFile = gfs.createFile(ss.getInputStream());
 
-        // Report identifier
-        return getReport(gfsFile);
+            // Set meta data
+            gfsFile.setFilename(documentName);
+            gfsFile.setContentType(contentType);
+
+            // Save and persist
+            gfsFile.save();
+
+            // Report identifier
+            return getReport(gfsFile);
+        } catch (Throwable ex) {
+            throw new XPathException(this, ex);
+        }
 
     }
 
@@ -133,7 +145,7 @@ public class Store extends BasicFunction {
         // Some identities
         add(builder, "id", "" + gfsFile.getId());
         add(builder, "filename", gfsFile.getFilename());
-        
+
         List<String> aliases = gfsFile.getAliases();
         if (aliases != null) {
             for (String alias : gfsFile.getAliases()) {
@@ -142,17 +154,17 @@ public class Store extends BasicFunction {
         }
 
         // mimetype
-        add(builder, "contentType",gfsFile.getContentType());
-        
+        add(builder, "contentType", gfsFile.getContentType());
+
         // sizes
         add(builder, "length", "" + gfsFile.getLength());
         add(builder, "chunkSize", "" + gfsFile.getChunkSize());
         add(builder, "numberOfChunks", "" + gfsFile.numChunks());
-        
+
         // more meta data
         add(builder, "uploadDate", "" + (new DateTimeValue(gfsFile.getUploadDate()).getStringValue()));
         add(builder, "md5", gfsFile.getMD5());
-        
+
         // finish root element
         builder.endElement();
 
@@ -166,6 +178,18 @@ public class Store extends BasicFunction {
             builder.startElement("", elementName, elementName, null);
             builder.characters(value);
             builder.endElement();
+        }
+
+    }
+
+    private String getMimeType(Sequence inputValue, String filename) throws XPathException {
+
+        if (inputValue.hasOne()) {
+            return inputValue.itemAt(0).getStringValue();
+        } else {
+
+            MimeType mime = MimeTable.getInstance().getContentTypeFor(filename);
+            return mime.getName();
         }
 
     }
