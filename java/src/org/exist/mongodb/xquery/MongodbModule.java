ab9b30b18f38d7834f1f16d83fecad551095583b
Dannes Wessels <dannes@exist-db.org>
2015-02-01 21:43:35 +0100

    Added save function

--- java/src/org/exist/mongodb/xquery/MongodbModule.java
+++ java/src/org/exist/mongodb/xquery/MongodbModule.java
@@ -19,6 +19,7 @@ import org.exist.mongodb.xquery.mongodb.collection.FindOne;
 import org.exist.mongodb.xquery.mongodb.collection.Group;
 import org.exist.mongodb.xquery.mongodb.collection.Insert;
 import org.exist.mongodb.xquery.mongodb.collection.MapReduce;
+import org.exist.mongodb.xquery.mongodb.collection.Save;
 import org.exist.mongodb.xquery.mongodb.db.EvalCommand;
 import org.exist.mongodb.xquery.mongodb.db.ListCollections;
 import org.exist.xquery.AbstractInternalModule;
@@ -36,39 +37,40 @@ public class MongodbModule extends AbstractInternalModule {
     public final static String RELEASED_IN_VERSION = "eXist-2.2";
 
     public final static FunctionDef[] functions = { 
-        new FunctionDef(ListMongdbClientIds.signatures[0], ListMongdbClientIds.class),   
-        new FunctionDef(ListDatabases.signatures[0], ListDatabases.class),
-        new FunctionDef(ListCollections.signatures[0], ListCollections.class),
+        new FunctionDef(Aggregate.signatures[0], Aggregate.class), 
+        new FunctionDef(Close.signatures[0], Close.class),
         new FunctionDef(Connect.signatures[0], Connect.class),
+        new FunctionDef(Count.signatures[0], Count.class),
+        new FunctionDef(Count.signatures[1], Count.class),   
         new FunctionDef(Drop.signatures[0], Drop.class),
+        new FunctionDef(EvalCommand.signatures[0], EvalCommand.class),
+        new FunctionDef(EvalCommand.signatures[1], EvalCommand.class),
+        new FunctionDef(EvalCommand.signatures[2], EvalCommand.class),
         new FunctionDef(Find.signatures[0], Find.class),
         new FunctionDef(Find.signatures[1], Find.class),
         new FunctionDef(Find.signatures[2], Find.class),
+        new FunctionDef(FindAndModify.signatures[0], FindAndModify.class),
+        new FunctionDef(FindAndModify.signatures[1], FindAndModify.class),
+        new FunctionDef(FindAndRemove.signatures[0], FindAndRemove.class),
         new FunctionDef(FindOne.signatures[0], FindOne.class),
         new FunctionDef(FindOne.signatures[1], FindOne.class),
         new FunctionDef(FindOne.signatures[2], FindOne.class),
         new FunctionDef(FindOne.signatures[3], FindOne.class),
-        new FunctionDef(FindAndModify.signatures[0], FindAndModify.class),
-        new FunctionDef(FindAndModify.signatures[1], FindAndModify.class),
-        new FunctionDef(FindAndRemove.signatures[0], FindAndRemove.class),
-        new FunctionDef(EvalCommand.signatures[0], EvalCommand.class),
-        new FunctionDef(EvalCommand.signatures[1], EvalCommand.class),
-        new FunctionDef(EvalCommand.signatures[2], EvalCommand.class),
+        new FunctionDef(Group.signatures[0], Group.class), 
         new FunctionDef(Insert.signatures[0], Insert.class),
-        new FunctionDef(Close.signatures[0], Close.class),
-        new FunctionDef(Count.signatures[0], Count.class),
-        new FunctionDef(Count.signatures[1], Count.class),   
-        new FunctionDef(Aggregate.signatures[0], Aggregate.class), 
+        new FunctionDef(ListCollections.signatures[0], ListCollections.class),
+        new FunctionDef(ListDatabases.signatures[0], ListDatabases.class),
+        new FunctionDef(ListMongdbClientIds.signatures[0], ListMongdbClientIds.class),   
         new FunctionDef(MapReduce.signatures[0], MapReduce.class),
         new FunctionDef(Remove.signatures[0], Remove.class), 
-        new FunctionDef(Group.signatures[0], Group.class), 
+        new FunctionDef(Save.signatures[0], Save.class),
     };
     
     public final static ErrorCode MONG0001 = new MongodbErrorCode("MONG0001", "Forbidden");
     public final static ErrorCode MONG0002 = new MongodbErrorCode("MONG0002", "Mongodb exception");
     public final static ErrorCode MONG0003 = new MongodbErrorCode("MONG0003", "Generic exception");
     public final static ErrorCode MONG0004 = new MongodbErrorCode("MONG0004", "JSON Syntax exception");
-    public final static ErrorCode MONG0005 = new MongodbErrorCode("MONG0005", "Command Failure exception");
+    public final static ErrorCode MONG0005 = new MongodbErrorCode("MONG0005", "Command exception");
 
     public final static QName EXCEPTION_QNAME
             = new QName("exception", MongodbModule.NAMESPACE_URI, MongodbModule.PREFIX);
---
+++ java/src/org/exist/mongodb/xquery/mongodb/collection/Save.java
@@ -0,0 +1,131 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2014 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.exist.mongodb.xquery.mongodb.collection;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCommandException;
+import com.mongodb.MongoException;
+import com.mongodb.WriteResult;
+import com.mongodb.util.JSON;
+import com.mongodb.util.JSONParseException;
+import org.exist.dom.QName;
+import static org.exist.mongodb.shared.FunctionDefinitions.PARAMETER_COLLECTION;
+import static org.exist.mongodb.shared.FunctionDefinitions.PARAMETER_DATABASE;
+import static org.exist.mongodb.shared.FunctionDefinitions.PARAMETER_MONGODB_CLIENT;
+import org.exist.mongodb.shared.MongodbClientStore;
+import org.exist.mongodb.xquery.MongodbModule;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.StringValue;
+import org.exist.xquery.value.Type;
+
+/**
+ * Functions to save a document in mongodb
+ *
+ * @author Dannes Wessels
+ */
+public class Save extends BasicFunction {
+
+    private static final String save = "save";
+    
+        public static final String PARAM_JSONCONTENT = "content";
+    public static final String DESCR_JSONCONTENT = "Document content as JSON formatted document";
+
+    public static final FunctionParameterSequenceType PARAMETER_JSONCONTENT
+            = new FunctionParameterSequenceType(PARAM_JSONCONTENT, Type.STRING, Cardinality.ONE, DESCR_JSONCONTENT);
+    
+    public final static FunctionSignature signatures[] = {
+        new FunctionSignature(
+        new QName(save, MongodbModule.NAMESPACE_URI, MongodbModule.PREFIX), "Update an existing document or insert a document depending on the parameter. "
+                + "If the document does not contain an '_id' field, then the method performs an insert with the specified fields in the document as well "
+                + "as an '_id' field with a unique objectId value. If the document contains an '_id' field, then the method performs an upsert querying the collection on the '_id' field: " +
+"If a document does not exist with the specified '_id' value, the method performs an insert with the specified fields in the document. " +
+"If a document exists with the specified '_id' value, the method performs an update, replacing all field in the existing record with the fields from the document.",
+        new SequenceType[]{
+            PARAMETER_MONGODB_CLIENT, PARAMETER_DATABASE, PARAMETER_COLLECTION, PARAMETER_JSONCONTENT},
+        new FunctionReturnSequenceType(Type.STRING, Cardinality.ONE, "The write result, JSON formatted")
+        ),};
+
+    public Save(XQueryContext context, FunctionSignature signature) {
+        super(context, signature);
+    }
+
+    @Override
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+
+        try {
+            String mongodbClientId = args[0].itemAt(0).getStringValue();
+            String dbname = args[1].itemAt(0).getStringValue();
+            String collection = args[2].itemAt(0).getStringValue();
+
+            // Check id
+            MongodbClientStore.getInstance().validate(mongodbClientId);
+
+            // Get Mongodb client
+            MongoClient client = MongodbClientStore.getInstance().get(mongodbClientId);
+
+            // Get database
+            DB db = client.getDB(dbname);
+            DBCollection dbcol = db.getCollection(collection);
+            
+            // Get data
+            BasicDBObject data = (BasicDBObject) JSON.parse(args[3].itemAt(0).getStringValue());
+    
+            // Execute save
+            WriteResult result = dbcol.save(data);
+
+            return new StringValue(result.toString());
+            
+        } catch (MongoCommandException ex){
+            // TODO return as value?
+            LOG.error(ex.getMessage(), ex);
+            throw new XPathException(this, MongodbModule.MONG0005, ex.getMessage());
+
+        } catch (JSONParseException ex) {
+            String msg = "Invalid JSON data: " + ex.getMessage();
+            LOG.error(msg);
+            throw new XPathException(this, MongodbModule.MONG0004, msg);
+
+        } catch (XPathException ex) {
+            LOG.error(ex.getMessage(), ex);
+            throw new XPathException(this, ex.getMessage(), ex);
+
+        } catch (MongoException ex) {
+            LOG.error(ex.getMessage(), ex);
+            throw new XPathException(this, MongodbModule.MONG0002, ex.getMessage());
+
+        } catch (Throwable t) {
+            LOG.error(t.getMessage(), t);
+            throw new XPathException(this, MongodbModule.MONG0003, t.getMessage());
+        }
+
+    }
+
+}
