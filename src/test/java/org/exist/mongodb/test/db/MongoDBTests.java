aa77edd3cfa6544eccbfb6cf06a2cd7a428c82c0
Dannes Wessels <dizzzz@users.noreply.github.com>
2018-05-03 08:49:18 +0200

    Update MongoDBTests.java

--- src/test/java/org/exist/mongodb/test/db/MongoDBTests.java
+++ src/test/java/org/exist/mongodb/test/db/MongoDBTests.java
@@ -1,19 +1,13 @@
 package org.exist.mongodb.test.db;
 
-import org.apache.commons.lang3.StringUtils;
-import xquery.TestRunner;
+import org.exist.test.runner.XSuite;
+import org.junit.runner.RunWith;
 
-public class MongoDBTests extends TestRunner {
+@RunWith(XSuite.class)
+@XSuite.XSuiteFiles({
+        "src/test/xquery"
+})
 
-    @Override
-    protected String getDirectory() {
-        
-        ClassLoader loader = this.getClass().getClassLoader();
-        String className = this.getClass().getCanonicalName().replaceAll("\\.", "\\/");
-        
-        String fullPath = loader.getResource(className + ".class").getFile();
-        String directory = StringUtils.substringBeforeLast(fullPath, "/");
-          
-        return directory;
-    }
+public class MongoDBTests {
+
 }
